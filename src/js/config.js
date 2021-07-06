cad1dd130d3b293d6d71191672d17a7190e7c27f
nikolatun <niki_tc@abv.bg>
2021-07-06 17:59:04 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 12;
+export const KEY = 'e6d9a0b9-dd91-43d8-a3fb-ddba08014ad3';
+export const MODAL_CLOSE_SEC = 2.5;
