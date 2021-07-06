670b15efdff0c1cc2f86214d662c948e30ca157a
nikolatun <niki_tc@abv.bg>
2021-07-06 18:10:27 +0300

    Added new feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -127,6 +127,10 @@ const controlAddRecipe = async function (newRecipe) {
 
 // Upload the new recipe data
 
+const newFeature = function () {
+  console.log('Welcome to app');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -135,6 +139,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
